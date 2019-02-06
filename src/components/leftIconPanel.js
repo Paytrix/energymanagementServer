5145d8e24c31ade232f64a4de254ef43e24a9c14
Kilian Mayer <paytrix@web.de>
2019-02-06 08:41:54 +0100

    adding onClick to IconPanel

--- src/components/leftIconPanel.js
+++ src/components/leftIconPanel.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import './style.css';
 
 export default class leftIconPanel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: "pageBoiler"
+    }
+  }
+
   render() {
     return (
       <div id="bodyleftIconPanel">
@@ -9,3 +16,13 @@ export default class leftIconPanel extends Component {
         <div id="leftPanelSolar" className="leftPanelIcon" onClick={this.changePage}></div>
         <div id="leftPanelPumpe" className="leftPanelIcon"></div>
         <div id="leftPanelHeizung" className="leftPanelIcon"></div>
+      </div>
+    )
+  }
+
+  changePage () {
+    this.setState({
+      page: "pageSolar"
+    });
+  }
+}
